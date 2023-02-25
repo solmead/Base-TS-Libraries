9709e6871277e238c184f4925552f84dd1574a5a
solmead <solmead@gmail.com>
2023-02-24 21:56:07 -0500

    Test

--- src/resolve.ts
+++ src/resolve.ts
@@ -5,8 +5,10 @@ var System:any = System || {};
 (function () {
     const endsWithFileExtension = /\/?\.[a-zA-Z]{2,}$/;
     const originalResolve = System.constructor.prototype.resolve;
+
     // tslint:disable-next-line:only-arrow-functions
     System.constructor.prototype.resolve = function () {
+      this.test = "";
       // apply original resolve to make sure importmaps are resolved first
       const url = originalResolve.apply(this, arguments);
       // append .js file extension if url is missing a file extension
