8708843de8516c0a64200f2ec784e6ad8c769c06
solmead <solmead@gmail.com>
2023-02-24 21:59:11 -0500

    Update

--- src/resolve.ts
+++ src/resolve.ts
@@ -2,13 +2,13 @@
 // tslint:disable-next-line:no-var-keyword
 var System:any = System || {};
 
+// tslint:disable-next-line:only-arrow-functions
 (function () {
     const endsWithFileExtension = /\/?\.[a-zA-Z]{2,}$/;
     const originalResolve = System.constructor.prototype.resolve;
 
     // tslint:disable-next-line:only-arrow-functions
     System.constructor.prototype.resolve = function () {
-      this.test = "";
       // apply original resolve to make sure importmaps are resolved first
       const url = originalResolve.apply(this, arguments);
       // append .js file extension if url is missing a file extension
